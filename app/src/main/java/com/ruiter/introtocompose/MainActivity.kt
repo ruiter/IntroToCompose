39e0cf3e5bdabcb1375cfb99c4da308d306b6ad8
Ruiter Matos <ruitercomp@gmail.com>
2022-01-14 16:08:03 -0300

    mutable state of delegate with by

--- app/src/main/java/com/ruiter/introtocompose/MainActivity.kt
+++ app/src/main/java/com/ruiter/introtocompose/MainActivity.kt
@@ -11,7 +11,7 @@ import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -46,11 +46,13 @@ fun MyApp() {
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(style = TextStyle(
-                color = Color.White,
-                fontSize = 30.sp,
-                fontWeight = FontWeight.Bold
-            ), text = "$100")
+            Text(
+                style = TextStyle(
+                    color = Color.White,
+                    fontSize = 30.sp,
+                    fontWeight = FontWeight.Bold
+                ), text = "$100"
+            )
             Spacer(modifier = Modifier.height(40.dp))
             CreateCircleCard()
         }
@@ -60,16 +62,23 @@ fun MyApp() {
 @Preview
 @Composable
 fun CreateCircleCard() {
+    var moneyCounter by remember {
+        mutableStateOf(0)
+    }
+
     Card(
         modifier = Modifier
             .padding(4.dp)
             .size(85.dp)
-            .clickable { Log.i("ruiter", "CreateCircleCard: tap ") },
+            .clickable {
+                Log.i("ruiter", "CreateCircleCard: tap ")
+                moneyCounter += 1
+            },
         shape = CircleShape,
         elevation = 4.dp
     ) {
         Box(contentAlignment = Alignment.Center) {
-            Text(text = "Tap")
+            Text(text = "Tap $moneyCounter")
         }
     }
 }
